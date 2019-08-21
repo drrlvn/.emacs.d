712d80bc03804f476e505529ec2f044ecbe4c9ad
Dror Levin <spatz@psybear.com>
2019-08-21 16:42:46 +0300

    Add minimal CI using GitHub Actions

---
+++ .ci.el
@@ -0,0 +1,9 @@
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(push "~/.emacs.d/lisp" load-path)
