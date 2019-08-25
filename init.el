df402cbe2de25e80838ed2dad3f3a13447edb063
Roey Darwish Dror <roey.ghost@gmail.com>
2019-08-25 10:36:50 +0300

    Save buffers without asking when performing magit actions (#97)

--- init.el
+++ init.el
@@ -774,6 +774,7 @@
         magit-section-initial-visibility-alist '((recent . show)
                                                  (unpushed . show)
                                                  (unpulled . show))
+        magit-save-repository-buffers 'dontask
         magit-status-initial-section '(((unstaged) (status)) ((untracked) (status)))
         magit-revision-show-gravatars '("^Author:     " . "^Commit:     "))
   (remove-hook 'magit-pre-display-buffer-hook #'magit-save-window-configuration)
